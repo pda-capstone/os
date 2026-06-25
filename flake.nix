082d76edba7f3d7e842235fda16697f9fa09b793
Tanner Weber <tannerw@pdx.edu>
2026-06-25 15:54:07 -0700

    feat: nix flake dev shell

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Flake for PostmarketOS image creation";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs systems (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.nixfmt);
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.pmbootstrap
+          ];
+          shellHook = ''
+            echo "In dev shell!"
+          '';
+        };
+      });
+    };
+}
